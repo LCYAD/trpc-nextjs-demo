3d2970092fbffa554070bb1a07e0496632b28406
Adrian Lee <alee.lcagency@gmail.com>
2023-07-14 10:08:00 +0800

    feat: api read from JSON file (#8)

--- src/pages/api/persons/[position].ts
+++ src/pages/api/persons/[position].ts
@@ -4,14 +4,14 @@ import { type Person } from '@/schemas/data';
 import { ErrorRes } from '@/types/api';
 import { removePerson } from '@/utils/data';
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Person[] | ErrorRes>,
 ) {
   if (req.method === 'DELETE') {
     const position = parseInt(req.query.position as any, 10);
     try {
-      const updatedPersons = removePerson(position);
+      const updatedPersons = await removePerson(position);
       res.status(200).json(updatedPersons);
     } catch (err: any) {
       res.status(400).json({
--- src/pages/api/persons/index.ts
+++ src/pages/api/persons/index.ts
@@ -2,29 +2,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { type Person } from '@/schemas/data';
 import { ErrorRes } from '@/types/api';
-import { addPerson, clearPersons, getPersons } from '@/utils/data';
+import { addPerson, clearPersons, getPersonsFromJson } from '@/utils/data';
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Person[] | ErrorRes>,
 ) {
   if (req.method === 'GET') {
-    const currPersons = getPersons();
+    const currPersons = await getPersonsFromJson();
     res.status(200).json(currPersons);
   } else if (req.method === 'POST') {
     try {
       const body =
         typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
-      const updatedPersons = addPerson(body);
+      const updatedPersons = await addPerson(body);
       res.status(201).json(updatedPersons);
     } catch (err: any) {
+      console.log(err);
       res.status(400).json({
         message: 'Cannot Add Person',
         reasons: err.errors,
       });
     }
   } else if (req.method === 'DELETE') {
-    const updatedPersons = clearPersons();
+    const updatedPersons = await clearPersons();
     res.status(200).json(updatedPersons);
   } else {
     res.status(404).json({
